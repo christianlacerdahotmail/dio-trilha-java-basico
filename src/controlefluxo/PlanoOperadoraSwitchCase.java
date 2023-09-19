69a5a0c4b6a36a1b5adec326520003c03e1c81ee
Chrístian <christianlacerda@hotmail.com>
2023-09-19 17:02:30 -0300

    Introdução e Estruturas Condicionais com Java

---
+++ src/controlefluxo/PlanoOperadoraSwitchCase.java
@@ -0,0 +1,21 @@
+package controlefluxo;
+
+public class PlanoOperadoraSwitchCase {
+
+	public static void main(String[] args) {
+		String plano = "T";
+
+		switch (plano) {
+			case "T": {
+				System.out.println("5Gb Youtube");
+		}
+			case "M": {
+				System.out.println("Whats e Instagram grátis");
+		}
+			case "B": {
+				System.out.println("100 minutos de ligação");
+			}
+		}
+	}
+
+}
