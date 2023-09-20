7c83125d2d7ce8660b7a6d5f1c18e2b0f048e3ce
Chrístian <christianlacerda@hotmail.com>
2023-09-20 17:44:34 -0300

    estrutura de repetição

---
+++ src/controlefluxo/ExemploForArray.java
@@ -0,0 +1,14 @@
+package controlefluxo;
+
+public class ExemploForArray {
+
+	public static void main(String[] args) {
+		
+		// em arrays o indice inicia sempre em ZERO
+		String alunos [] = {"FELIPE", "JONAS", "JULIA", "MARCOS" };
+		
+		for(String aluno : alunos) {
+			System.out.println("Nome do aluno é: " + aluno);
+		}
+	}
+}
